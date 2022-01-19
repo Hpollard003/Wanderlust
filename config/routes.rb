528eb338f5a27af6d058a14c31f370ab8b03f5c5
Hpollard003 <hpollard003@gmail.com>
2022-01-19 02:55:44 -0500

    Commit message

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :invitations 
-  end
-  resources :journals do
-    resources :pages
-  end
-  
-  
-  
-  
-  # Everything below is a custom route that get specific actions from specific controllers
-  post "/invites/:user_id" , to: "invitations#follow"
-  post "/signup", to: "users#create"
-  get "/me", to: "users#profile"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  patch "/users/:id/upload-image", to: "users#upload", as: :upload
-   
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
