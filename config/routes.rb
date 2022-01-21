2f48f66cd9923ce08faed0156841b965774156b4
Hpollard003 <hpollard003@gmail.com>
2022-01-21 17:46:48 -0500

    Update Refresh Bug Add Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :users do
     resources :invitations 
   end
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   patch "/users/:id/upload-image", to: "users#upload", as: :upload
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
